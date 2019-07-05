7317627cf19505337f566de369174f88717a077c
shahoian <ruben.shahoyan@cern.ch>
2019-07-05 17:30:20 +0200

    initial commit

---
+++ KMCUtils.cxx
@@ -0,0 +1,58 @@
+#include "KMCUtils.h"
+#include <TMath.h>
+//---------------------------------
+ClassImp(MagField)
+
+
+MagField::MagField(UInt_t id) {
+  SetUniqueID(id);
+  fZMin[0] = 0;
+  fZMax[0] = 40;
+  if (MagField::kNReg>1) {
+    fZMin[1] = 350;
+    fZMax[1] = 650;
+  }
+  double magf[2][3] = {{-20.,0,0},{200.,30.,300.}};
+  for (int i=0; i<MagField::kNReg; i++) {
+    for (int j=0; j<3; j++) {
+      fBVal[i][j] = magf[i][j];
+    }
+  }
+}
+
+//__________________________________________________
+void MagField::Field(const Double_t *xyz, Double_t *bxyz) 
+{
+  bxyz[0]=bxyz[1]=bxyz[2]=0.;
+  double R = sqrt(xyz[0] * xyz[0] + xyz[1] * xyz[1]);
+  for (int ir=0;ir<kNReg;ir++) {
+    if((xyz[2] >fZMin[ir]) && (xyz[2] < fZMax[ir])) {
+      if(ir == 0) for (int i=3;i--;) bxyz[i] = fBVal[ir][i];
+      //    if(ir == 2) for (int i=3;i--;) bxyz[i] = fBVal[ir][i];
+      if(ir == 1){
+	//	double R = sqrt(xyz[0] * xyz[0] + xyz[1] * xyz[1]);
+	if(R > fBVal[ir][1] && R < fBVal[ir][2]){
+	  // Atlas/Chorus toroid
+	  //	  bxyz[0] = -fBVal[ir][0] * xyz[1]/R;
+	  //	  bxyz[1] =  fBVal[ir][0] * xyz[0]/R;
+          //bxyz[0] = -fBVal[ir][0] * xyz[1]/R - 0.001 * xyz[1]/R/R;
+          //bxyz[1] =  fBVal[ir][0] * xyz[0]/R + 0.001 * xyz[0]/R/R;
+
+	  // ACM toroid
+	  // direct polarity
+	  bxyz[0] = -fBVal[ir][0] * xyz[1]/R/R;
+	  bxyz[1] =  fBVal[ir][0] * xyz[0]/R/R;
+	  // reverse polarity
+ 	  //bxyz[0] =  fBVal[ir][0] * xyz[1]/R/R;
+ 	  //bxyz[1] = -fBVal[ir][0] * xyz[0]/R/R;
+	  bxyz[2] = 0.;
+	}
+      }
+      //if(R<400.) 
+      //      printf("ir = %d z=%f Bx=%f By=%f Bz=%f\n",ir,xyz[2],bxyz[0],bxyz[1],bxyz[2]);
+      //if (xyz[2]<0) printf("ir = %d z=%f Bx=%f By=%f Bz=%f\n",ir,xyz[2],bxyz[0],bxyz[1],bxyz[2]);
+      return;
+    }
+  }
+  //  printf("z=%f Bx=%f By=%f Bz=%f\n",xyz[2],bxyz[0],bxyz[1],bxyz[2]);
+}
---
+++ KMCUtils.h
@@ -0,0 +1,32 @@
+#ifndef KMCUTILS_H
+#define KMCUTILS_H
+
+#include <TGeoGlobalMagField.h>
+
+//==========================================================================
+class MagField: public TVirtualMagField
+{
+ public:
+  enum {kNReg=2};
+  MagField(UInt_t id);
+  virtual ~MagField() {}
+  virtual void Field(const Double_t *xyz, Double_t *bxyz);
+  //
+  virtual int GetNReg() const {return (int)kNReg;}
+  virtual const double* GetZMin() const {return fZMin;}
+  virtual const double* GetZMax() const {return fZMax;}
+  const double* GetBVals(int ir) const {return &fBVal[ir][0];} 
+  virtual void SetZMin(int nreg, double zmin) { fZMin[nreg] = zmin; }
+  virtual void SetZMax(int nreg, double zmin) { fZMax[nreg] = zmin; }
+  virtual void SetBVals(int nreg, int index, double val) { fBVal[nreg][index] = val; }
+
+ protected:
+  double fZMin[kNReg]; // min z of each field region
+  double fZMax[kNReg]; // max z of each field region
+  double fBVal[kNReg][3]; // field values
+  //
+  ClassDef(MagField, 1) // custom magfield
+};
+
+
+#endif
